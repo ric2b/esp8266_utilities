04253ce1a540f01cb3be737183a0fb6cf5b794ed
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2017-02-04 16:09:04 +0000

    Now using deep sleep function to save power.

--- BTC_EUR/BTC_EUR.ino
+++ BTC_EUR/BTC_EUR.ino
@@ -21,17 +21,14 @@ const char* post_data = "pair=XXBTZEUR\r\n";
 // Use web browser to view and copy SHA1 fingerprint of the certificate.
 const char* fingerprint = "E6:DD:9B:E8:9E:2A:29:80:B0:E8:67:28:46:1C:90:08:3D:2A:AE:FF";
 
-const int sleepTimeS = 10;
+const int sleepTimeS = 60;
 
 void setup(void)
 { 
   /* U8g2 Project: KS0108 Test Board */
-  pinMode(16, OUTPUT);
-  digitalWrite(16, 0);	
-  
-  u8x8.begin();
-  u8x8.setPowerSave(0);
-  
+  //pinMode(16, OUTPUT);
+  //digitalWrite(16, 0);	
+   
   /* WiFi */
   Serial.begin(115200);
   Serial.println();
@@ -46,10 +43,7 @@ void setup(void)
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-}
 
-void loop(void)
-{
   // Use WiFiClientSecure class to create TLS connection
   WiFiClientSecure client;
   Serial.print("connecting to ");
@@ -101,11 +95,21 @@ void loop(void)
   else
     Serial.println("Correctly parsed the payload");
 
+  //u8x8.begin();
+  u8x8.initDisplay();
+  u8x8.setPowerSave(0);
+  u8x8.setContrast(30);
+
   u8x8.setFont(u8x8_font_chroma48medium8_r);
   u8x8.drawString(0,1,"BTC/EUR");
   u8x8.drawString(0,2,price);
 
   Serial.println("ESP8266 going to sleep mode");
-  delay(sleepTimeS * 1000);
-  //ESP.deepSleep(sleepTimeS * 1000000);
+  // Must connect D0 to RST. When it wakes up, the device resets and memory is lost.
+  ESP.deepSleep(sleepTimeS * 1000000);
+}
+
+void loop(void)
+{
+  
 }
