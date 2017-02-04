20aa08c5018125b722fb0786b44459bbefead605
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2017-02-04 01:44:03 +0000

    Can now fetch the BTC/EUR Ticker price and print it to the screen

--- BTC_EUR/BTC_EUR.ino
+++ BTC_EUR/BTC_EUR.ino
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include <U8x8lib.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ArduinoJson.h>
 
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
@@ -7,8 +10,6 @@
 
 U8X8_SSD1306_64X48_ER_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);   // EastRising 0.66" OLED breakout board, Uno: A4=SDA, A5=SCL, 5V powered
 
-#include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
 
 const char* ssid = ".......";
 const char* password = ".......";
@@ -36,7 +37,7 @@ void setup(void)
   u8x8.setPowerSave(0);
   
   /* WiFi */
-    Serial.begin(115200);
+  Serial.begin(115200);
   Serial.println();
   Serial.print("connecting to ");
   Serial.println(ssid);
@@ -65,13 +66,17 @@ void setup(void)
     Serial.println("certificate doesn't match");
   }
 
-  String url = "/0/public/Time";
+  String url = "/0/public/Ticker";
   Serial.print("requesting URL: ");
   Serial.println(url);
 
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
-               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "User-Agent: BTC_EUR_Ticker_ESP8266\r\n" +
+               "Content-Type: application/x-www-form-urlencoded\r\n" + 
+               "Content-Length: 13\r\n" + 
+               "\r\n" +
+               "pair=XXBTZEUR\r\n" + 
                "Connection: close\r\n\r\n");
 
   Serial.println("request sent");
@@ -82,24 +87,33 @@ void setup(void)
       break;
     }
   }
-  String line = client.readStringUntil('\n');
-  if (line.startsWith("{\"state\":\"success\"")) {
-    Serial.println("esp8266/Arduino CI successfull!");
-  } else {
-    Serial.println("esp8266/Arduino CI has failed");
-  }
+  
+  String contentLength = client.readStringUntil('\n');
+  DynamicJsonBuffer jsonBuffer(300);
+  String payload = client.readStringUntil('\n');
+  JsonObject& root = jsonBuffer.parseObject(payload);
+
+  if (!root.success())
+    Serial.println("parseObject() failed");
+  const char* price = root["result"]["XXBTZEUR"]["c"][0];
+  
   Serial.println("reply was:");
   Serial.println("==========");
-  Serial.println(line);
-  Serial.println(client.readStringUntil('\n'));
+  Serial.println(price);
   Serial.println("==========");
   Serial.println("closing connection");
+
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.drawString(0,1,"BTC/EUR");
+  u8x8.drawString(0,2,price);
+  u8x8.refreshDisplay();
 }
 
 void loop(void)
 {
   u8x8.setFont(u8x8_font_chroma48medium8_r);
-  u8x8.drawString(0,1,"Hello World!");
+  u8x8.drawString(0,1,"BTC/EUR");
+  //u8x8.drawString(0,2,price);
   u8x8.refreshDisplay();		// for SSD1606  
   delay(1000);
   
